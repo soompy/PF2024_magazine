ff4bc24498387f4cbbf60775137484e14e315302
이수민 <sooper1137@gmail.com>
2024-09-04 15:14:04 +0900

    메뉴영역 dim 추가

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   const btnMenu = document.querySelector(".btn-menu");
   const gnbWrapper = document.querySelector(".gnb-wrapper");
+  const dimLayer = document.querySelector(".dim");
   const body = document.body;
 
   btnMenu.addEventListener("click", function () {
     btnMenu.classList.toggle("on");
     gnbWrapper.classList.toggle("on");
+    dimLayer.classList.toggle("on");
 
     if (gnbWrapper.classList.contains("on")) {
       body.style.overflow = "hidden";
